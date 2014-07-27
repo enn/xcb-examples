687c8ecaf02552aa164614e6c42c7f9486534828
enn <enn@users.noreply.github.com>
2014-07-28 00:38:39 +0100

    Update window_event.c
    bug "fix"

--- window_event.c
+++ window_event.c
@@ -1,66 +1,108 @@
-// gcc `pkg-config --libs xcb` main.c -o main
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include <xcb/xcb.h>
 
-xcb_connection_t *connection;
-xcb_screen_t *screen;
+xcb_connection_t    *c;
+xcb_screen_t        *screen;
+
+/* win */
+xcb_drawable_t       win;
+
+/* stard/end point storage for line drawing */
+xcb_point_t line_start;
+xcb_point_t line_end;
+
+
+xcb_drawable_t create_sub_window(xcb_drawable_t parent, xcb_gravity_t gravity) {
+  
+  xcb_drawable_t swin;
+  uint32_t mask = 0;
+  uint32_t values4[4];
+
+  swin = xcb_generate_id(c);
+  mask = XCB_CW_BACK_PIXEL | XCB_CW_BORDER_PIXEL | XCB_CW_WIN_GRAVITY | XCB_CW_EVENT_MASK;
+  values4[0] = screen->white_pixel;
+  values4[1] = screen->black_pixel;
+  values4[2] = gravity;
+  values4[3] = XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_KEY_PRESS;
+  xcb_create_window (c,
+                     XCB_COPY_FROM_PARENT,
+                     swin,
+		     win,
+                     0, 0,
+                     50, 50,
+                     2,
+                     XCB_WINDOW_CLASS_INPUT_OUTPUT,
+                     screen->root_visual,
+                     mask, values4);
+  return swin;
+}
 
 void create_window() {
-  // http://www.x.org/releases/current/doc/xproto/x11protocol.html#requests:CreateWindow
-  // http://www.x.org/archive/current/doc/man/man3/xcb_create_window.3.xhtml
-  //
-  // N.B. xcb's order corresponds to the order of the wire.
-  // You can look at the protocol encoding: http://www.x.org/releases/current/doc/xproto/x11protocol.html#Encoding::Requests
   
-  uint32_t mask;
-  uint32_t values[2];
-  
-  xcb_window_t window;
-  xcb_void_cookie_t cookie;
-  
+  uint32_t             mask = 0;
+  uint32_t             values[3];
+
+  win = xcb_generate_id(c);
   mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
   values[0] = screen->white_pixel;
   values[1] = XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_KEY_PRESS;
-  
-  window = xcb_generate_id(connection);
-  cookie = xcb_create_window(connection,
-			     XCB_COPY_FROM_PARENT, window, screen->root,
-			     0, 0, 640, 480,
-			     0,
-			     XCB_WINDOW_CLASS_INPUT_OUTPUT,
-			     screen->root_visual,
-			     mask, values);
-  
-  xcb_map_window(connection, window);
+  xcb_create_window (c,
+		     screen->root_depth,
+                     win,
+                     screen->root,
+                     0, 0,
+                     54,54, /* border size big enough for the subwindows to fit in */
+                     0,
+                     XCB_WINDOW_CLASS_INPUT_OUTPUT,
+                     screen->root_visual, 
+                     mask, values);
+
+  xcb_map_window (c, win);
 }
 
+
 void event_loop() {
-  xcb_generic_event_t *event;
-  
-  while(event = xcb_wait_for_event(connection)) {
-    switch(event->response_type) {
-    case XCB_EXPOSE:
-      puts("expose");
+  xcb_generic_event_t *e;
+  while ((e = xcb_wait_for_event (c))) {
+    switch (e->response_type & ~0x80) {
+
+      /* ESC to exit */
+    case XCB_KEY_PRESS: {
+      xcb_key_press_event_t *ev;
+      ev = (xcb_key_press_event_t *)e;
+      if (ev->detail == 9) return;
       break;
-    case XCB_KEY_PRESS:
-      return;
     }
+
+    }
+    free (e);
   }
+  
 }
 
 int main(void) {
-  connection = xcb_connect(NULL, NULL); // Callers need to use xcb_connection_has_error() to check for failure.
-  screen = xcb_setup_roots_iterator(xcb_get_setup(connection)).data;
-  
+  int i;
+  /* Open the connection to the X server */
+  c = xcb_connect (NULL, NULL);
+  /* Get the first screen */
+  screen = xcb_setup_roots_iterator (xcb_get_setup (c)).data;
+  /* make the main window */  
   create_window();
+
+  /* Create a subwindow for each gravity type */
+  create_sub_window(win, XCB_GRAVITY_NORTH_WEST);
+  create_sub_window(win, XCB_GRAVITY_NORTH_EAST);
+  create_sub_window(win, XCB_GRAVITY_SOUTH_WEST);
+  create_sub_window(win, XCB_GRAVITY_SOUTH_EAST);
   
-  xcb_flush(connection);
-  
+  /* map the subwindows */
+  xcb_map_subwindows(c, win);
+  xcb_flush(c);
+
   event_loop();
+
   puts("bye!");
-  
   return EXIT_SUCCESS;
 }
